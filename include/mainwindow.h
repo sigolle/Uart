b4928f27de27e0436122bd76bbba33f8871f8107
sigolle <741257621@qq.com>
2024-03-03 21:31:24 +0800

    add UartSetting GUI 1

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -2,7 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QComboBox>
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
@@ -19,5 +22,16 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QComboBox *cbbUart[5];
+    void addComboBoxContent();
+
+//UI
+private:
+    QLabel *lbUartSetting;
+    QLabel *lbUart;
+    QLabel *lbBaud;
+    QLabel *lbData;
+    QLabel *lbCheck;
+    QLabel *lbStop;
 };
 #endif // MAINWINDOW_H
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -7,6 +7,32 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
+    QVBoxLayout *vblUart = new QVBoxLayout(this);
+
+    QStringList lbNames{"串口","波特率","数据位","校验位","停止位"};
+    QLabel *lbUartSetting = new QLabel("串口设置");
+    unsigned char cbbIndex=0;
+    vblUart->addWidget(lbUartSetting);
+    for(const QString &name : lbNames)
+    {
+        QLabel *label = new QLabel(name);
+        cbbUart[cbbIndex] = new QComboBox;
+        cbbUart[cbbIndex]->setEditable(true);
+        QHBoxLayout *hblUart = new QHBoxLayout(this);
+        hblUart->addWidget(label);
+        hblUart->addWidget(cbbUart[cbbIndex++]);
+        vblUart->addLayout(hblUart);
+    }
+    ui->centralwidget->setLayout(vblUart);
+    addComboBoxContent();
+
+}
+
+void MainWindow::addComboBoxContent()
+{
+    cbbUart[1]->addItem("9600");
+    cbbUart[1]->addItem("19200");
+    cbbUart[1]->addItem("115200");
 }
 
 MainWindow::~MainWindow()
